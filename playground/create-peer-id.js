53bea226ff10683cde91d58a229e04cacb2ebb61
Philippe Schommers <philippe@schommers.be>
2021-08-16 21:27:03 +0200

    Remove playground

--- playground/create-peer-id.js
+++
@@ -1,7 +0,0 @@
-const { writeFile } = require("fs/promises");
-const PeerId = require("peer-id");
-
-(async () => {
-  const peerId = await PeerId.create({ keyType: "secp256k1" });
-  await writeFile("peer-id.json", JSON.stringify(peerId.toJSON()));
-})();
